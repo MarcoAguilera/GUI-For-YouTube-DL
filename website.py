fb5f4eb86dbf4741b0b06570ad3f5d733f1b1b6d
Daniel Ochoa Aguila <dochoa-aguila@csumb.edu>
2017-07-15 18:52:51 +0000

    option function added

--- website.py
+++ website.py
@@ -6,16 +6,28 @@ app = Flask(__name__)
 def mainPage():
     return render_template("mainscreen.html")
     
-@app.route('/', methods=['POST'])
+@app.route("/", methods=['POST'])
 def mainPagePost():
     html = request.form['html']
     print (html)
-
+    
+@app.route("/options")
+def optionPage():
+    return render_template("optionPage.html")
+    
+#@app.route("/option", Methods=["POST"])
+#def optionPagePost():
+    #
+    #
+    #
+    #
+    
 if __name__ == '__main__':
     app.run(
         debug=True,
         port=int(os.getenv('PORT', 8080)),
         host=os.getenv('IP', '0.0.0.0')
     )
+    
     
     
