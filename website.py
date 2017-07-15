4fbbc56d2a54edc1b2a50a80ff39f674e371dd22
Daniel Ochoa Aguila <dochoa-aguila@csumb.edu>
2017-07-15 18:09:58 +0000

    hello world with flask

---
+++ website.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=int(os.getenv('PORT', 8080)),
+        host=os.getenv('IP', '0.0.0.0')
+    )
