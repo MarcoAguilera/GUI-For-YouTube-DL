937c9269903d4813d1d06c38a3f6b3e1d943da95
Daniel Ochoa Aguila <dochoa-aguila@csumb.edu>
2017-07-22 17:28:41 +0000

    pages are getting linked

--- website.py
+++ website.py
@@ -10,15 +10,19 @@ def mainPage():
 def mainPagePost():
     html = request.form['html']
     print (html)
+    return redirect(url_for('optionPage'))
     
 @app.route("/option")
 def optionPage():
     return render_template("select.html")
     
-@app.route("/option", methods=['GET','POST'])
+@app.route("/option", methods=['POST'])
 def optionPagePost():
+    print ('in post')
     html = request.form.get('videoQuality')
+    print ("got it")
     print (str(html))
+    return redirect(url_for('mainPage'))
     
 if __name__ == '__main__':
     app.run(
@@ -26,6 +30,4 @@ if __name__ == '__main__':
         port=int(os.getenv('PORT', 8080)),
         host=os.getenv('IP', '0.0.0.0')
     )
-    
-    
     
