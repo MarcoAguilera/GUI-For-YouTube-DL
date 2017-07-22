919efb338c4c5f72ae2a0add1cd9eb973949ffb6
Marco Aguilera <maaguilera@csumb.edu>
2017-07-22 18:34:23 +0000

    Fixed function to retrieve video info

--- YTBack_End.py
+++ YTBack_End.py
@@ -1,16 +1,8 @@
-import sys
-import os
-import json
-import commands
+import subprocess
 
-def find(link):
-    os.system("youtube-dl " + link)
-    os.system("youtube-dl --list-formats " + link)
-    print ("sys.argv")
-    print sys.argv
-    print(commands.getstatusoutput("cat syscall_list.txt | grep f89e7000 | awk '{print $2}'"))
-    return
-
-link = "https://www.youtube.com/watch?v=JoTMpxG8Zwc"
-
-find(link)
+def get_info(link):
+ 
+    process = subprocess.Popen(['youtube-dl', '--list-formats', str(link)], stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    
+    return out    
