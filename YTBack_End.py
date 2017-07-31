7078755a8a0d96192dab44a1afea1eed253e5b3a
Marco Aguilera <maaguilera@csumb.edu>
2017-07-31 22:16:28 +0000

    Converted into a Class

--- YTBack_End.py
+++ YTBack_End.py
@@ -1,12 +1,23 @@
 import subprocess
-
-def store_info(link):
+class Media(object):
     
-    process = subprocess.Popen(['youtube-dl', '--list-formats', str(link)], stdout=subprocess.PIPE)
-    out, err = process.communicate()
-    print("PRINTING")
-    file = open("formats.txt", "w+")
-    file.write(out)
-    file.close()
-    return out
-
+    #Uses this constructor to instanciate an object when a link is entered
+    def __init__(self,link):
+        self.link = link
+    
+    #After a link has been set, use this function to gather information about the link    
+    def set_info(self):
+        self.process = subprocess.Popen(['youtube-dl', '--list-formats', self.link], stdout=subprocess.PIPE)
+        self.out, self.err = self.process.communicate()
+        file = open("formats.txt", "w+")
+        file.write(self.out)
+        file.close()
+    
+    #Used to store format code for later use    
+    def set_format_code(self,code):
+        self.format_code = code
+    
+    #Used to download on to computer
+    def download_media(self):
+        subprocess.Popen(['youtube-dl', '-f', str(self.format_code), self.link])
+        
