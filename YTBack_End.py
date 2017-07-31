c6db00f847a010625491a22bd6749cbfd78d3310
Marco Aguilera <maaguilera@csumb.edu>
2017-07-31 19:44:07 +0000

    Added a text file

--- YTBack_End.py
+++ YTBack_End.py
@@ -1,8 +1,12 @@
 import subprocess
 
-def get_info(link):
- 
+def store_info(link):
+    
     process = subprocess.Popen(['youtube-dl', '--list-formats', str(link)], stdout=subprocess.PIPE)
     out, err = process.communicate()
-    
-    return out    
+    print("PRINTING")
+    file = open("formats.txt", "w+")
+    file.write(out)
+    file.close()
+    return out
+
